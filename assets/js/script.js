c27a064dd4b148943941b22bee80c5e67a0f5a71
Carolyn <cghudson@uwalumni.com>
2022-04-09 10:27:13 -0500

    js pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
 var apiKey = "2f2b2008dbc13afbf4925de60c195419";
 
+//search for city
+//display current conditions of city
+    //display city name, date, icon representation of conditions, temperature, humidity, wind speed, UV index
+    //display color to indicate if weather is favorable, moderate, or severe
+//display future conditions for city
+    //display 5 day forecast, date, icon representation of weather conditions, temperature, wind speed, humidigy
+//save search in local storage
+//display saved searches
+    //when clicked, user is presesented with current and future weather conditions
